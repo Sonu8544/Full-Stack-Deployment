cf4b01e4c0f96bf5ff927468d458ad57a346e547
Sonu8544 <sksonu8544@gmail.com>
2025-11-22 18:49:30 +0530

    setough backend course policy whitelist url

--- server/index.js
+++ server/index.js
@@ -4,7 +4,22 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 
-const app = express(); 
+const app = express();
+
+// setough cors policy dev and production domains
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+      //   Add your production domain here
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -13,7 +28,6 @@ app.use(cors());
 app.get("/api/message", (req, res) => {
   res.send({ message: "Hello from server..." });
 });
-
 
 const PORT = process.env.PORT || 8888;
 
