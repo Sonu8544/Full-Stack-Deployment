e8b82477aaeccec1d3d62caa8cb2a0c71c9eb028
Sonu8544 <sksonu8544@gmail.com>
2025-11-22 18:54:58 +0530

    setough frontend and read bata from backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:7777/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <>
-      <div className="text-3xl font-bold underline">
+      <div className="text-3xl font-bold text-center mt-10 text-slate-700">
         Full Stack Project Deplloyment
       </div>
+      <p className="text-center mt-5 text-slate-700 text-2xl">
+        Message: {message}
+      </p>
     </>
   );
 };
