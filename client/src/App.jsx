5c3ae90594928ff8e37f351059ca057cafcd1de5
Sonu8544 <sksonu8544@gmail.com>
2025-11-22 18:38:29 +0530

    setough frontend and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        Full Stack Project Deplloyment
+      </div>
+    </>
+  );
+};
+
+export default App;
